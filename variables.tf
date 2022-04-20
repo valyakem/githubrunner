ee9a574016fc2a19359889cd4ecb375366158412
Valentine AKEM <valyakem@yahoo.co.uk>
2022-04-20 08:22:23 -0400

    add more

--- variables.tf
+++ variables.tf
@@ -7,30 +7,35 @@ variable "github_app_id" {
   default = "nbrunners"
 }
 
-variable "vpcname" {
-description = "name of vpc"
-default = "githubrunner-vpc"
+variable "name" {
+  description = "the name of your stack, e.g. \"demo\""
+  default     = "githubrunner-vpc"
+}
+
+variable "environment" {
+  description = "the name of your environment, e.g. \"prod\""
+  default     = "test"
+}
+
+variable "cidr" {
+  description = "The CIDR block for the VPC."
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "List of public subnets"
+  default     = ["10.0.16.0/20", "10.0.48.0/20", "10.0.80.0/20"]
+}
+
+variable "private_subnets" {
+  description = "List of private subnets"
+  default     = ["10.0.0.0/20", "10.0.32.0/20", "10.0.64.0/20"]
 }
 
 variable "availability_zones" {
-  description = "a comma-separated list of availability zones, defaults to all AZ of the region, if set to something other than the defaults, both private_subnets and public_subnets have to be defined as well"
+  description = "List of availability zones"
   default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
-
-# variable "cidr" {
-#   description = "The CIDR block for the VPC."
-#   default     = "10.0.0.0/16"
-# }
-
-# variable "private_subnets" {
-#   description = "a list of CIDRs for private subnets in your VPC, must be set if the cidr variable is defined, needs to have as many elements as there are availability zones"
-#   default     = ["10.0.0.0/20", "10.0.32.0/20", "10.0.64.0/20"]
-# }
-
-# variable "public_subnets" {
-#   description = "a list of CIDRs for public subnets in your VPC, must be set if the cidr variable is defined, needs to have as many elements as there are availability zones"
-#   default     = ["10.0.16.0/20", "10.0.48.0/20", "10.0.80.0/20"]
-# }
 
 #---------------------
 variable "aws_region" {
