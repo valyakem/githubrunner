418394d02bd9da593c9ad6c70c79f7275b5b4575
Valentine AKEM <valyakem@yahoo.co.uk>
2022-04-20 08:02:53 -0400

    change vpc resources

--- vpc.tf
+++ vpc.tf
@@ -2,11 +2,11 @@ module "vpc" {
   source  = "../../"
 
   name = "vpc-${local.environment}"
-  cidr = "10.0.0.0/16"
+  cidr = "172.31.0.0/16"
 
   azs             = ["${local.aws_region}a", "${local.aws_region}b", "${local.aws_region}c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["172.31.32.0/20", "172.31.0.0/20", "172.31.80.0/20"]
+  public_subnets  = ["172.31.16.0/20", "172.31.48.0/20", "172.31.64.0/20"]
 
   enable_dns_hostnames    = true
   enable_nat_gateway      = true
